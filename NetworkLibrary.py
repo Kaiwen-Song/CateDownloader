435e9b3b49d506fe92b15b973f9242064ea4f0d9
Kaiwen Song <kaiwen0602@gmail.com>
2016-12-21 12:34:44 +0000

    initial empty project

---
+++ NetworkLibrary.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from re import *
+import os
+
+Cate_URL = "https://cate.doc.ic.ac.uk"
+Download_Dir = "/Downloads"
+
+
+def create_directory(directory_name):
+    os.mkdir(directory_name)
+
+def navigate_to_directory(directory_name):
+    os.cd()
+
+def download_file_from_URL():
+
+
+
+def identify_files_to_download():
+    
+    return []
